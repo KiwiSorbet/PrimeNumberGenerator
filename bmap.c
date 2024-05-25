98d2ae146e2483cfd97484a50927f7dc910f499e
ChristopherGhosn <christopher.ghosn@mail.mcgill.ca>
2024-05-24 23:40:33 -0400

    refactoring

--- bmap.c
+++ bmap.c
@@ -31,34 +31,6 @@ struct bmap* bmap_create(size_t length, bool init) {
     return bmap;
 }
 
-// Frees an associated bitmap from memory.
-void bmap_free(struct bmap* bmap) {
-    free(bmap->map);
-    free(bmap);
-    return;
-}
-
-// extends the length in bits of the bitmap in a bitmap struct
-enum err_code bmap_extend(struct bmap* bmap, size_t extend_length, bool init) {
-    // create the new larger bitmap
-    size_t new_byte_length = bmap->length / 8 + (extend_length + 7) / 8;
-    uint8_t* new_map = malloc(new_byte_length);
-    if (new_map == NULL)
-        return FAILURE;
-
-    // copy contents of the old bitmap into the new one
-    memset(new_map + bmap->length / 8, (init == true) ? 0b11111111 : 0b00000000,
-           new_byte_length - bmap->length / 8);
-    memcpy(new_map, bmap->map, bmap->length / 8);
-
-    // assign new bitmap to the bitmap struct
-    free(bmap->map);
-    bmap->map = new_map;
-    bmap->length = new_byte_length * 8;
-
-    return SUCCESS;
-}
-
 // Prints the contents of the associated bitmap.
 void bmap_print(struct bmap* bmap) {
     size_t byte_length = (bmap->length + 7) / 8;

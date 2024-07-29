21dffdc40b91da24873414ee0740732897449c6a
KiwiSorbet <galaxygum51@gmail.com>
2024-07-29 02:03:41 -0400

    added command line argument for the number of primes to generate

--- main.c
+++ main.c
@@ -6,12 +6,18 @@
 #include "bmap.h"
 #include "main.h"
 
-#define BMAP_INCREMENT 10000000 // bitmap is incrementally extended
-#define LIST_SIZE 1000000 // how many prime numbers to find
+#define DEFAULT_LIST_SIZE 1000000 // how many prime numbers to find
 
-int main() {
+int main(int argc, char* argv[]) {
+    // get list size from command line arguments
+    size_t list_size;
+    if (argc == 2)
+        sscanf(argv[1], "%zu", &list_size);
+    else
+        list_size = DEFAULT_LIST_SIZE;
+
     // create bitmap
-    struct bmap* prime_map = bmap_create(BMAP_INCREMENT, true);
+    struct bmap* prime_map = bmap_create(list_size, true);
     if (prime_map == NULL)
         return -1;
 
@@ -21,13 +27,13 @@ int main() {
     size_t bmap_index = 1;
 
     // create the list of prime numbers
-    prime* prime_list = malloc(LIST_SIZE * sizeof(prime));
+    prime* prime_list = malloc(list_size * sizeof(prime));
     if (prime_list == NULL)
         return -1;
     size_t list_index = 0;
 
     // find prime numbers until list is filled
-    for (; list_index < LIST_SIZE; list_index++) {
+    for (; list_index < list_size; list_index++) {
         // find the next prime number
         bmap_index = bmap_find_next(prime_map, ++bmap_index, true, RIGHT);
 
@@ -35,7 +41,7 @@ int main() {
         if (bmap_index == prime_map->length) {
             // extend bitmap
             size_t old_length = prime_map->length; // save old length of bitmap
-            bmap_extend(prime_map, BMAP_INCREMENT, true);
+            bmap_extend(prime_map, list_size, true);
 
             // update new bitmap entries with previous primes
             for (size_t i = 0; i < list_index; i++)
@@ -51,7 +57,7 @@ int main() {
     }
 
     // print last prime number in the list
-    printf("%zuth prime number: %zu\n", list_index, prime_list[LIST_SIZE - 1]);
+    printf("%zuth prime number: %zu\n", list_index, prime_list[list_size - 1]);
     free(prime_list);
     bmap_free(prime_map);
 }

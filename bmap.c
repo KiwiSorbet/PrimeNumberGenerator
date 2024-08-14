ea7f33b204085dbfc35833d959c50916cdfb4384
KiwiSorbet <galaxygum51@gmail.com>
2024-08-13 22:03:47 -0400

    moved function documentation to header file

--- bmap.c
+++ bmap.c
@@ -10,7 +10,6 @@
 #define bmap_int uint64_t
 #define int_size sizeof(bmap_int)
 
-// Creates and initializes a bitmap of the input length in bits.
 struct bmap* bmap_create(size_t length, bool init) {
     // allocate memory for bitmap struct
     struct bmap* bmap = malloc(sizeof(struct bmap));
@@ -34,7 +33,6 @@ struct bmap* bmap_create(size_t length, bool init) {
     return bmap;
 }
 
-// Prints the contents of the associated bitmap.
 void bmap_print(const struct bmap* bmap) {
     size_t byte_length = (bmap->length + 7) / 8;
 
@@ -55,13 +53,11 @@ void bmap_print(const struct bmap* bmap) {
     return;
 }
 
-// Frees the associated bitmap from memory.
 void bmap_free(struct bmap* bmap) {
     free(bmap->map);
     free(bmap);
 }
 
-// Extends the length in bits of the bitmap in a bitmap struct.
 void bmap_extend(struct bmap* bmap, size_t extend_length, bool init) {
     // create the new larger bitmap
     size_t new_byte_length = bmap->length / 8 + (extend_length + 7) / 8;
@@ -80,7 +76,6 @@ void bmap_extend(struct bmap* bmap, size_t extend_length, bool init) {
     bmap->length = new_byte_length * 8;
 }
 
-// Returns the value of a certain location in the associated bitmap.
 bool bmap_get(const struct bmap* bmap, size_t index) {
     // index out of bounds always returns false
     if (index >= bmap->length)
@@ -97,10 +92,6 @@ bool bmap_get(const struct bmap* bmap, size_t index) {
     return (binary_control == 0) ? false : true;
 }
 
-// Finds the first occurence of a certain value in the bitmap from a starting
-// index (including the starting index) by walking through the bitmap in a
-// certain direction and returns its associated index. Returns the length of the
-// bitmap if no occurence was found.
 size_t bmap_find_next(const struct bmap* bmap, size_t index, bool value,
                       enum direction dir) {
     size_t cursor = index;
@@ -121,7 +112,6 @@ size_t bmap_find_next(const struct bmap* bmap, size_t index, bool value,
     return cursor;
 }
 
-// Sets a specific location in the associated bitmap to a specific value.
 void bmap_set(struct bmap* bmap, size_t index, bool value) {
     // index out of bounds
     if (index >= bmap->length)
@@ -143,8 +133,6 @@ void bmap_set(struct bmap* bmap, size_t index, bool value) {
     }
 }
 
-// Sets a number cnt of specific bits from a starting index in the associated
-// bitmap to a specific value.
 void bmap_set_mul(struct bmap* bmap, size_t index, size_t cnt, bool value) {
     for (size_t i = index; i < index + cnt; i++)
         bmap_set(bmap, i, value);
